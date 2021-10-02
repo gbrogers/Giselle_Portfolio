8d69b5c06c0bee6fcd0bc67d342321973b53f4f6
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-10-01 20:46:47 -0600

    graphQL for project queries set up

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,56 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { Link, graphql } from "gatsby";
+// import ProjectOne from '../components/ProjectOne/ProjectOne'
 
-export default function Projects() {
+export default function Projects({ data }) {
+  const projects = data.allMarkdownRemark.edges;
   return (
     <Layout title="Giselle's Projects">
       <Seo title="Projects" />
-      <div>This is the Project Page</div>
+
+      {projects.map(project => {
+        console.log(project.node.frontmatter);
+        return true;
+      })}
     </Layout>
   );
 }
+
+export const pageQuery = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            description
+            projecttech
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+//       nodes {
+//         excerpt
+//         fields {
+//           slug
+//         }
+//         frontmatter {
+//           date(formatString: "MMMM DD, YYYY")
+//           title
+//           description
+//         }
+//       }
+//     }
+//   }
+// `;
