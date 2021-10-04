abeb4479fa0dc88da268db7d3b7cbe2bb9064cd6
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-10-04 11:16:34 -0600

    contact page fix

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,12 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
-import * as styles from "../style.scss";
 
 export default function Contact() {
   return (
     <Layout title="Giselle's Projects">
       <Seo title="Projects" />
-      <div className={styles.filler}></div>
+      <div style={{ height: "50px" }}></div>
     </Layout>
   );
 }
