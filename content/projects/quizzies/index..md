dc122730fdfe2b02b677046bce886c23299864c7
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-10-04 11:03:56 -0600

    project page styled

--- content/projects/quizzies/index..md
+++ content/projects/quizzies/index..md
@@ -2,7 +2,6 @@
 title: Quizzies
 description: Quizzies is a web application using vanilla javascript that makes Axios calls to Open Trivia DB API, which is an external open source API that generates trivia questions. This application allows the user to select a quiz category, difficulty, and number of questions so that users can polish their trivia skills.
 projecttech: JavaScript | HTML5 | SCSS | Axios
-featuredImage: ../../images/Drinkz_home.PNG
+sourceCode: https://github.com/gbrogers/Quizzies
+demoVideo: https://vimeo.com/612848644
 ---
-
-![](../../../src/images/Drinkz_home.PNG)
--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { StaticQuery, graphql } from "gatsby";
 // import SingleProject from "../SingleProject/SingleProject";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 import * as styles from "./ProjectList.module.scss";
 
 export const ProjectList = () => (
@@ -16,6 +16,8 @@ export const ProjectList = () => (
                 description
                 projecttech
                 title
+                sourceCode
+                demoVideo
               }
             }
           }
@@ -23,15 +25,56 @@ export const ProjectList = () => (
       }
     `}
     render={data => (
-      <div className="projectPageContainer">
+      <div className={styles.projectPageContainer}>
+        {/* <h1 className={styles.projectHeader}>My Projects</h1> */}
         {data.allMarkdownRemark.edges.map(({ node }) => {
           return (
             <div className={styles.singleProjectPageContainer}>
-              {console.log(node.rawMarkDownBody)}
-              <GatsbyImage image={getImage(node.rawMarkDownBody)}></GatsbyImage>
-              <h4>{node.frontmatter.title}</h4>
-              <p>{node.frontmatter.description}</p>
-              <p>{node.frontmatter.projecttech}</p>
+              <div className={styles.headerPicture}>
+                {node.frontmatter.title == "Quizzies" && (
+                  <StaticImage
+                    className={styles.projectPhoto}
+                    src="../../images/Quizzies_5right.2.PNG"
+                    alt="mbmh home image"
+                    placeholder="blurred"
+                    width={400}
+                    height={250}
+                  ></StaticImage>
+                )}
+                {node.frontmatter.title == "Drinkz!" && (
+                  <StaticImage
+                    className={styles.projectPhoto}
+                    src="../../images/Drinkz_home.PNG"
+                    alt="mbmh home image"
+                    placeholder="blurred"
+                    width={400}
+                  ></StaticImage>
+                )}
+                {node.frontmatter.title == "My Body My Health" && (
+                  <StaticImage
+                    className={styles.projectPhoto}
+                    src="../../images/mbmh_home.PNG"
+                    alt="mbmh home image"
+                    placeholder="blurred"
+                    width={400}
+                  ></StaticImage>
+                )}
+              </div>
+              <div className={styles.singleProjectLabel}>
+                <h2 className={styles.projectTitle}>
+                  {node.frontmatter.title}
+                </h2>
+                <p>{node.frontmatter.description}</p>
+                <p>{node.frontmatter.projecttech}</p>
+                <div className={styles.projectLinks}>
+                  <a href={node.frontmatter.sourceCode} target="_blank">
+                    Source Code
+                  </a>
+                  <a href={node.frontmatter.demoVideo} target="_blank">
+                    Demo
+                  </a>
+                </div>
+              </div>
             </div>
           );
         })}
